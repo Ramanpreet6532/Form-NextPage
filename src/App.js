df17972380be019fd9e1d4190c3502074ae03a95
Ramanpreet6532 <rammugill72@gmail.com>
2020-08-18 16:19:47 +0530

    added routing for pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import './App.css';
-import Form from "././Components/Form"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from "./pages/Home"
+import Login from "./Components/Login"
+import SignUp from "./Components/SignUp"
 
 function App() {
   return (
     <div className="App">
-       <Form />
+       <div className="container">
+          <Router>
+             <nav>
+               <ul>
+                 <li><a href="pages/Home.js">HOME</a></li>
+                 <li a href="#">SIGNUP</li>
+                 <li a href="#">LOGIN</li>
+               </ul>
+
+             </nav>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/Signup">
+             <SignUp />
+          </Route>
+          <Route exact path="/Login">
+             <Login />
+          </Route>
+          </Router>
+       </div>
+
+      
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function(){
+    return(
+        <div className="container">
+           <h1>WELCOME HERE</h1>
+           <h2>We provide services in the field of web developement</h2>
+        </div>
+    )
+}
